9d9f0023f34d2bc8bc9daef53140527401e93663
Anurag Modi <modi.anurag1992@gmail.com>
2020-09-23 20:11:00 +0530

    Convert / to use os module for path

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         #'DIRS': [str(BASE_DIR.joinpath('templates'))],
-        'DIRS': [str(BASE_DIR+'/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
